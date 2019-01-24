3389f9db1c6f20f6e407ffc8ac15e308b7d241bd
Marcus Östling <mpt.ostling@gmail.com>
2019-01-24 21:54:41 +0100

    Add new feature

--- src/main/java/gradle/test/App.java
+++ src/main/java/gradle/test/App.java
@@ -8,6 +8,10 @@ public class App {
         return "Hello world.";
     }
 
+    public int get1() {
+        return 1;
+    }
+
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
     }
--- src/test/java/gradle/test/AppTest.java
+++ src/test/java/gradle/test/AppTest.java
@@ -11,4 +11,14 @@ public class AppTest {
         App classUnderTest = new App();
         assertNotNull("app should have a greeting", classUnderTest.getGreeting());
     }
+
+    @Test public void testAppHasAGreeting2() {
+        App classUnderTest = new App();
+        assertEquals("Hello world.", classUnderTest.getGreeting());
+    }
+
+    @Test public void testAppHasGet12() {
+        App classUnderTest = new App();
+        assertEquals(1, classUnderTest.get1());
+    }
 }
